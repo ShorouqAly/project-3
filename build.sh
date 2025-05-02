b4dde7401d5c88e9f1c99fd3794c2f54cb587183
ShorouqAly <ShorouqAly@users.noreply.github.com>
2025-05-02 08:56:35 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y ffmpeg
+
+# Download and extract static FFmpeg
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
+tar -xf ffmpeg.tar.xz
+cd ffmpeg-*-amd64-static
+
+# Move the binaries into the /usr/local/bin directory (which should be writable on Render at runtime)
+mkdir -p ../ffmpeg-bin
+cp ffmpeg ffprobe ../ffmpeg-bin/
+cd ..
+
+# Make sure the binaries are executable
+chmod +x ffmpeg-bin/ffmpeg ffmpeg-bin/ffprobe
+
+# Export path so your app can use it later
+export PATH=$PWD/ffmpeg-bin:$PATH
+
+# Continue with Python setup
+pip install -r requirements.txt
